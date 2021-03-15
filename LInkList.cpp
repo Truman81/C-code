bb8a31303e085761881273167008640ed38bf54d
YCX <1805190004@qq.com>
2021-03-15 16:46:21 +0800

    Laptop Code

---
+++ LInkList.cpp
@@ -0,0 +1,13 @@
+void createLinkList(LNode *&head)
+{
+    LNode *p;
+    L=(LNode*)malloc(sizeof(LNode));
+    A->next=B; B->next=C;
+    scr>>
+}
+
+typedef struct LNode()
+{
+    int data;
+    struct LNode *p;
+}LNode;
